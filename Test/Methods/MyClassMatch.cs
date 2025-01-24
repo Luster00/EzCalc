8acaf5403b8ee298b0f4ff2654897354aa72e2f6
silov <silov@SILOV>
2025-01-24 13:40:56 +0300

    Добавьте файлы проекта.

---
+++ Test/Methods/MyClassMatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Methods
+{
+
+    public class MyClassMatch
+    {
+        public static (string Message, double Value) MyOperations(double x, double y, char s)
+        {
+            if (s ==  '1')
+            {
+                return ("Сумма равна:", x + y); 
+            }
+
+            else if (s == '2')
+            {
+                return ("Разность равна:", x - y);
+            }
+
+            else if (s == '3')
+            {
+                return ("Произведение равно:", x * y);
+            }
+
+            else if (s == '4')
+            {
+                return ($"Частное равно:", x / y);
+            }
+            else
+            {
+                return ("Неверный знак", 0);
+            }
+            
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using Test.Methods;
+
+Console.WriteLine("Добро пожаловать в калькулятор!");
+
+while (true)
+{
+    Console.WriteLine("Введите х: ");
+    double x = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите y: ");
+    double y = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Выберите оператор: \n1. + \n2. -, \n3. *\n4. /");
+    char symbol = Convert.ToChar(Console.ReadLine());
+
+    var result = MyClassMatch.MyOperations(x, y, symbol);
+    Console.WriteLine($"{result.Message} {result.Value}");
+
+    Console.WriteLine("Для продолжения введите: y \nДля выхода введите: n");
+    string input = Console.ReadLine();
+    if (input == "n")
+    {
+        Console.Clear();
+        Console.WriteLine("Выход из программы");
+        Console.ReadLine();
+        break;
+    }
+    else if (input == "y")
+    {
+        Console.Clear();
+        continue;
+    }
+}
+
+
+
+
+
+
